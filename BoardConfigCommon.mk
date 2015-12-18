861863f7e889d543ad0970582eb22c65150a0ba8
electrikjesus <electrikjesus@gmail.com>
2015-12-18 09:59:31 -0800

    Added empty bliss stuff

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -94,3 +94,20 @@ TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 BOARD_SEPOLICY_DIRS += \
         device/asus/grouper/sepolicy
 
+# BlissPop Configs
+#TARGET_TC_ROM := 4.9
+#TARGET_TC_KERNEL := 4.9
+#BLISSIFY := true
+#BLISS_O3 := true
+#BLISS_STRICT := false
+#BLISS_GRAPHITE := true
+#BLISS_KRAIT := false
+#BLISS_PIPE := true
+#TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+#TARGET_KERNEL_HEADER_ARCH := arm
+#WITH_LZMA_OTA := true
+#TARGET_ENABLE_UKM := true
+
+#SaberMod
+#-include vendor/bliss/config/sm.mk
